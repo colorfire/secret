4b4fcabba14e44fb6767da71627885bcbd6dbbc8
Weigang <weigangqiu@google.com>
2013-05-06 21:22:34 +0800

    get 265 site content through url.

--- Tools/urls.py
+++ Tools/urls.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python
 
+import sys
 import codecs
 import re
 import urllib
-import HTMLParser
+import json
+from bs4 import BeautifulSoup
 
 #fl = codecs.open('265.html', encoding='gb2312')
-fl = urllib.urlopen('http://www.265.com/Qiche_Qipei/')
-s = fl.read()
-urls = re.findall(r'<a(.+?)href=(?:"|\')?((?:https?://|/)[^\'"]+)(?:"|\')?(.*?)>(.+?)</a>',s.decode('gb2312'))
-for url in urls:
-    print '{"label":"%s", "href":"%s"},' % (url[3], url[1])
-
-
-class HtmlStructureHandle(HTMLParser.HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print tag
-    def handle_endtag(self, tag):
-        print tag
-    def handle_data(self, data):
-        print data
-
+#fl = urllib.urlopen('http://www.265.com/Qiche_Qipei/')
+#s = fl.read()
+#urls = re.findall(r'<a(.+?)href=(?:"|\')?((?:https?://|/)[^\'"]+)(?:"|\')?(.*?)>(.+?)</a>',s.decode('gb2312'))
+#for url in urls:
+#    print '{"label":"%s", "href":"%s"},' % (url[3], url[1])
 
 def parseWithReg(s):
     urls = re.findall(r'<a(.+?)href=(?:"|\')?((?:https?://|/)[^\'"]+)(?:"|\')?(.*?)>(.+?)</a>',s)
@@ -30,8 +22,44 @@ def parseWithReg(s):
     return result
 
 def parseWithLib(s):
-    parser = new HtmlStructureHandle()
-    return parset.feed(s)
+    subs = []
+    soup = BeautifulSoup(s)
+    for content in soup.find_all('div', class_='catalogCategory'):
+        sub = {}
+        cs = BeautifulSoup(content.prettify())
+        title = cs.find('div', class_='titleCS')
+
+        links = []
+        ul = cs.find('ul', class_='listUrl')
+        us = BeautifulSoup(ul.prettify())
+        for link in us.find_all('a'):
+            tmp = {'label':_trim(link.string), 'href':link['href']}
+            links.append(tmp)
+
+        relates = []
+        dl = cs.find('dl', class_='SoKeys')
+        ds = BeautifulSoup(dl.prettify())
+        for dd in ds.find_all('a'):
+            tmp = {'label':_trim(dd.string), 'href':dd['href']}
+            relates.append(tmp)
+        sub['title'] = {'label':_trim(title.string)}
+        sub['links'] = links
+        sub['relate'] = relates
+        subs.append(sub)
+    result = {'id':'s', 'subs':subs}
+    return result
+
+def _trim(string):
+    return string.replace('\n', '').strip()
 
 if __name__ == '__main__':
     print 'start parse script.'
+    #fl = codecs.open('shipin.html', encoding='gb2312')
+    #s = fl.read()
+    if len(sys.argv) < 1:
+        print 'Error Parameter.'
+        sys.exit(0)
+    fl = urllib.urlopen(sys.argv[1])
+    s = fl.read().decode('gb2312')
+    print json.dumps(parseWithLib(s))
+    print 'end parse script.'
